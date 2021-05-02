59dbe9147bbb78c6d380909ebd96f60c35f4c105
Duc Nguyen <44717375+KirbyNguyen@users.noreply.github.com>
2021-05-02 16:41:57 -0700

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "426313f2f3133c2f20415344c4a22df3",
-"index.html": "4946752ea815feff913f669ae19c78dc",
-"/": "4946752ea815feff913f669ae19c78dc",
-"main.dart.js": "eedd8a044a6588d8adee6c18cd2516d6",
+"index.html": "c5e434092eb147b20468e3763bb8bc61",
+"/": "c5e434092eb147b20468e3763bb8bc61",
+"main.dart.js": "e74902d835d55cbaace6580054f52587",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
