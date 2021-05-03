12ab930514d9e57e50e62170dd5ffe856cde4cf5
Duc Nguyen <44717375+KirbyNguyen@users.noreply.github.com>
2021-05-02 17:41:49 -0700

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -19,8 +19,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "6258a0645052beb5bc28321a3ed9b96e",
-"/": "6258a0645052beb5bc28321a3ed9b96e",
+"index.html": "87934e9c24aea3b711fd944815692b28",
+"/": "87934e9c24aea3b711fd944815692b28",
 "main.dart.js": "3be48fa40c83c3384a9c0cd3cc0eb2cf",
 "manifest.json": "5a2c0526f1fe73e6f211bc7a9d15c210",
 "version.json": "426313f2f3133c2f20415344c4a22df3"
